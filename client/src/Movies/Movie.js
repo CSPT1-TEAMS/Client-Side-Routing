664ef30a650653f319afbf81455e4c9f556d29bf
Stephanie Brooks <sbrooks84591@hotmail.com>
2018-04-04 21:47:26 -0500

    SBrooks branch

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
+import  { Link } from 'react-router-dom';
 
-export default class MovieCard extends React.Component {
+export default class MovieCard extends Component {
   
   state = {
     movie: null,
@@ -9,7 +10,9 @@ export default class MovieCard extends React.Component {
 
   componentDidMount() {
     // change this line to grab the id passed on the URL
-    const id = 1;
+    const id = this.props.match.params.id;
+    console.log("id", id);
+
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
       .then(response => this.setState(() => ({ movie: response.data })))
@@ -22,10 +25,10 @@ export default class MovieCard extends React.Component {
     if(!this.state.movie) {
       return <div>Loading movie information...</div>
     }
-
     const { title, director, metascore, stars } = this.state.movie;
     return (
       <div className="movie-card">
+      <Link to="/">Home</Link>
         <h2>{title}</h2>
         <div className="movie-director">
           Director: <em>{director}</em>
@@ -40,7 +43,9 @@ export default class MovieCard extends React.Component {
             {star}
           </div>
         ))}
+        
       </div>
+            
     );
   }
 }
