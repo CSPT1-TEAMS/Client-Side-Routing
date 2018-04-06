7285faefbb5a827f3dd65ca3dff889191e9dee0b
Glenn-David Daniel <d2rd@outlook.com>
2018-04-05 21:39:52 -0700

    Cleared up link issue.  Styled subheaders

--- backlog.md
+++ backlog.md
@@ -1,3 +1,10 @@
 # Client-side Routing Project Backlog for Ala/David/Russell
 ### This doc is to track features we want to implement.
-* Stretch: import results from themovieDB search into server.js as json objects"
+
+## Issues:
+  * Format budget and revenue as currency.
+  * [done] Remove links from market stats
+  * Clean up class names in index.css to reflect this app. (ex: rename Note-item to MovieCard-item)
+
+## Stretch Goals: 
+  * Import results from themovieDB search into server.js as json objects"
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -33,32 +33,33 @@ export default class MovieList extends React.Component {
 function MovieCard({ movie }) {
   const { title, director, metascore, vote_average, vote_count, popularity, budget, revenue, stars } = movie;
   return (
-    <Link to={`/movies/${movie.id}`}>
-      <div className="Note-row">
-        <div className="Note-item">
+    <div className="Note-row">
+      <div className="Note-item">
+        <Link to={`/movies/${movie.id}`}>
           <div className="Note-header"><h2>{title}</h2>
           </div>
-          <div className="movie-director">
-            Director: <em>{director}</em>
+        </Link>            
+        <h4><div className="movie-director">
+          Director: <em>{director}</em> 
+        </div></h4>
+         <div className="Sub-Header">Cast</div>
+        {stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
           </div>
-          
-          <h4>Market Stats</h4>  {/* metascore, vote_average, vote_count, popularity, budget, revenue, */}
-          <div className="movie-metascore">Metascore: <strong>{metascore}</strong></div>
-          <div className="movie-popularity">Popularity: <strong>{popularity}</strong></div> 
-          <div className="movie-vote_average">Average Votes: <strong>{vote_average}</strong></div> 
-          <div className="movie-vote_count">Total Votes: <strong>{vote_count}</strong></div> 
-          <p></p>
-          <div className="movie-budget">Budget: <strong>{budget}</strong></div> 
-          <div className="movie-revenue">Revenue: <strong>{revenue}</strong></div>
+        ))}       
+        <div className="Sub-Header">Marketing Stats</div>  {/* metascore, vote_average, vote_count, popularity, budget, revenue, */}
+        <div className="movie-metascore">Metascore: <strong>{metascore}</strong></div>
+        <div className="movie-popularity">Popularity: <strong>{popularity}</strong></div> 
+        <div className="movie-vote_average">Average Votes: <strong>{vote_average}</strong></div> 
+        <div className="movie-vote_count">Total Votes: <strong>{vote_count}</strong></div> 
+        <p></p>
+        <div className="movie-budget">Budget: <strong>{budget}</strong></div> 
+        <div className="movie-revenue">Revenue: <strong>{revenue}</strong></div>
 
-          <h4>Cast</h4>
-          {stars.map(star => (
-            <div key={star} className="movie-star">
-              {star}
-            </div>
-          ))}
-        </div>
+
       </div>
-    </Link>
+    </div>
+
   );
 }
